d71107119f467be8fb0032a0d6ed0ae4e62eeb28
pythonhelp3 <128414238+pythonhelp3@users.noreply.github.com>
2023-04-05 20:56:01 -0500

    Updated main.py
    This is the link to the Skinport API if you do not already have it and some info about the API.

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from skinportassistbot import Bot
 from api import API
 
 
+# (Camden comment): Here is the link to the API on Skinport's website, I'm not sure how it's used or if it will work for this use application. If not, it could be a useful
+# tool for calculating profit if the item is relisted on each item since items under $1000 have a sales fee of 12% and items over $1000 have a sales fee of 6%.
 
+# Link: https://docs.skinport.com
